a6685e58f7dad56e38d20ce761900a3a444594d3
bikram.khyaju <khyajubikram@gmail.com>
2022-06-08 10:22:16 -0500

    new project given by kesha to create Note and create different api to retrieve them

---
+++ src/test/java/com/noteproject/noteProject/NoteProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noteproject.noteProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
